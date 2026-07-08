2ae7f316e00b02822ff6d8f7de319918fd4b6245
gleb <116607327+loeclos@users.noreply.github.com>
2026-07-07 18:43:21 -0700

    fix: fix some bugs, comments

--- agent/agent.go
+++ agent/agent.go
@@ -1,19 +1,17 @@
 package agent
 
 import (
-	"context"
-
 	"slices"
 
 	bifrost "github.com/maximhq/bifrost/core"
 	"github.com/maximhq/bifrost/core/schemas"
 )
 
+// The main Agent struct.
 type Agent struct {
 	Client        bifrost.Bifrost
 	Provider      schemas.ModelProvider
 	Model         string
-	Instructions  string
 	Messages      []schemas.ChatMessage
 	Tools         Tools
 	ToolSchemas   []schemas.ChatTool
@@ -38,8 +36,12 @@ type AsyncAgentErrorChunk struct {
 	Content string
 }
 
-func (a *Agent) Run(ctx context.Context, message string) (*schemas.BifrostChatResponse, *schemas.BifrostError) {
-	response, err := a.Client.ChatCompletionRequest(schemas.NewBifrostContext(ctx, schemas.NoDeadline), &schemas.BifrostChatRequest{
+// The Run function simply runs a one off interaction,
+// prompt -> reponse. No tools, and no agentic capabilities.
+// We might need to expand upon this sometime, but I feel that
+// simply creating a YOLO mode for the async agent will be sufficient.
+func (a *Agent) Run(ctx schemas.BifrostContext, message string) (*schemas.BifrostChatResponse, *schemas.BifrostError) {
+	response, err := a.Client.ChatCompletionRequest(&ctx, &schemas.BifrostChatRequest{
 		Provider: a.Provider,
 		Model:    a.Model,
 		Input: append(a.Messages,
@@ -49,9 +51,6 @@ func (a *Agent) Run(ctx context.Context, message string) (*schemas.BifrostChatRe
 					ContentStr: schemas.Ptr(message),
 				},
 			}),
-		Params: &schemas.ChatParameters{
-			Tools: a.ToolSchemas,
-		},
 	})
 
 	if err != nil {
@@ -61,155 +60,140 @@ func (a *Agent) Run(ctx context.Context, message string) (*schemas.BifrostChatRe
 	return response, nil
 }
 
+// The RunAsync function is the engine of the agent loop.
+// Mostly everything happens in here.
 func (a *Agent) RunAsync(ctx schemas.BifrostContext, message string) <-chan any {
 	chunkChan := make(chan any, 100)
 
-	if len(a.ToolSchemas) != len(a.Tools) {
-		panic("agent: amount of tools is unequal in ToolSchemas compared to Tools.")
-	}
-
 	// Run all agent stuff in a goroutine
 	// to make it async.
-
 	go func() {
-		for {
 
-			// Start a stream with Bifrost, using the agent settings and history.
+		// Start a stream with Bifrost, using the agent settings and history.
+		stream, err := a.Client.ChatCompletionStreamRequest(&ctx, &schemas.BifrostChatRequest{
+			Provider: a.Provider,
+			Model:    a.Model,
+			Input: append(a.Messages,
+				schemas.ChatMessage{
+					Role: schemas.ChatMessageRoleUser,
+					Content: &schemas.ChatMessageContent{
+						ContentStr: schemas.Ptr(message),
+					},
+				}),
+			Params: &schemas.ChatParameters{
+				Tools: a.ToolSchemas,
+			},
+		})
 
-			stream, err := a.Client.ChatCompletionStreamRequest(schemas.NewBifrostContext(context.Background(), schemas.NoDeadline), &schemas.BifrostChatRequest{
-				Provider: a.Provider,
-				Model:    a.Model,
-				Input: append(a.Messages,
-					schemas.ChatMessage{
-						Role: schemas.ChatMessageRoleUser,
-						Content: &schemas.ChatMessageContent{
-							ContentStr: schemas.Ptr(message),
-						},
-					}),
-				Params: &schemas.ChatParameters{
-					Tools: a.ToolSchemas,
-				},
-			})
-
-			if err != nil {
-				chunkChan <- AsyncAgentErrorChunk{
-					Content: err.String(),
-				}
+		if err != nil {
+			chunkChan <- AsyncAgentErrorChunk{
+				Content: err.String(),
 			}
 
-			for chunk := range stream {
-				if chunk.BifrostChatResponse != nil && len(chunk.BifrostChatResponse.Choices) > 0 {
-					choice := chunk.BifrostChatResponse.Choices[0]
+			return
+		}
 
-					// Check whether there is actual streaming content.
+		for chunk := range stream {
+			if chunk.BifrostChatResponse != nil && len(chunk.BifrostChatResponse.Choices) > 0 {
+				choice := chunk.BifrostChatResponse.Choices[0]
 
-					if choice.ChatStreamResponseChoice != nil &&
-						choice.ChatStreamResponseChoice.Delta != nil {
+				// Check whether there is any actual streaming content.
+				if choice.ChatStreamResponseChoice != nil &&
+					choice.ChatStreamResponseChoice.Delta != nil {
 
-						// If the stream content is basic reasoning or response
-						// text, return it to the client right away.
-
-						if choice.ChatStreamResponseChoice.Delta.Content != nil {
-							content := *choice.ChatStreamResponseChoice.Delta.Content
-							chunkChan <- AsyncAgentTextChunk{
-								Content: content,
-							}
+					// If the stream content is basic reasoning or response
+					// text, return it to the client right away.
+					if choice.ChatStreamResponseChoice.Delta.Content != nil {
+						content := *choice.ChatStreamResponseChoice.Delta.Content
+						chunkChan <- AsyncAgentTextChunk{
+							Content: content,
 						}
+					}
 
-						if choice.ChatStreamResponseChoice.Delta.Reasoning != nil {
-							content := *choice.ChatStreamResponseChoice.Delta.Reasoning
-							chunkChan <- AsyncAgentReasongingChunk{
-								Content: content,
-							}
+					if choice.ChatStreamResponseChoice.Delta.Reasoning != nil {
+						content := *choice.ChatStreamResponseChoice.Delta.Reasoning
+						chunkChan <- AsyncAgentReasongingChunk{
+							Content: content,
 						}
+					}
 
-						// If the streaming content involves tools, then we have
-						// a slightly different procedure.
+					// If the streaming content involves tools, then we have
+					// a slightly different procedure.
+					if len(choice.ChatStreamResponseChoice.Delta.ToolCalls) > 0 {
+						for _, tool := range choice.ChatStreamResponseChoice.Delta.ToolCalls {
 
-						if len(choice.ChatStreamResponseChoice.Delta.ToolCalls) > 0 {
-							for _, tool := range choice.ChatStreamResponseChoice.Delta.ToolCalls {
+							// If the tool is in the whitelist we skip
+							// checking whether it is in the blacklist.
+							// The whitelist overrules the blacklist.
+							if slices.Contains(a.ToolWhitelist, *tool.Function.Name) {
 
-								// If the tool is in the whitelist we skip
-								// checking whether it is in the blacklist.
-								// The whitelist overrules the blacklist.
+								// Create an ApproveToolChannel for each tool call,
+								// which will be used to approve that specific tool.
+								ApproveToolChannel := make(chan bool)
 
-								if slices.Contains(a.ToolWhitelist, *tool.Function.Name) {
-
-									// Create an ApproveToolChannel for each tool call,
-									// which will be used to approve that specific tool.
-
-									ApproveToolChannel := make(chan bool)
-
-									toolChunk := ToolChunk{
-										ID:              *tool.ID,
-										ToolName:        *tool.Function.Name,
-										ToolParams:      tool.Function.Arguments,
-										ApproveToolChan: ApproveToolChannel,
-									}
-
-									// Here we launch the ToolManager function. It takes
-									// in the toolChunk, the tool function, and the chunkChan.
-									// Using the ApproveToolChan we provide through the
-									// toolChunk, it will wait for a response from the
-									// main go routine (essentially the client or user)
-									// with a reject or approval of the tool. Following
-									// that, it will then run the tool and return the
-									// result through chunkChan which we provided.
-
-									go ToolManager(ctx, toolChunk, a.Tools[*tool.Function.Name], tool, chunkChan)
-
-									// After passing the tool into ToolManager, we can send
-									// it off to the client through chunkChan
-
-									chunkChan <- toolChunk
-
-								} else if slices.Contains(a.ToolBlacklist, *tool.Function.Name) || slices.Contains(a.ToolBlacklist, "*") {
-
-									// If the tool is in the blacklist (or the
-									// user added "*" to the blacklist, which
-									// automatically adds all tools), and
-									// not in the whitelist, then we send a
-									// AsyncAgentToolFailedChunk error to the client
-									// to tell them that the tool is blacklisted.
-
-									toolBlacklistedErrorChunk := ToolFailedChunk{
-										ID:         *tool.ID,
-										ToolName:   *tool.Function.Name,
-										ToolParams: tool.Function.Arguments,
-										Reason:     "tool in blacklist.",
-									}
-
-									chunkChan <- toolBlacklistedErrorChunk
-								} else {
-
-									// If the tool isn't found in either the blacklist
-									// or the whitelist, we simply send to back to the user.
-
-									ApproveToolChannel := make(chan bool)
-
-									toolChunk := ToolChunk{
-										ID:              *tool.ID,
-										ToolName:        *tool.Function.Name,
-										ToolParams:      tool.Function.Arguments,
-										ApproveToolChan: ApproveToolChannel,
-									}
-
-									// As above, we launch the ToolManager function. It takes
-									// in the toolChunk, the tool function, and the chunkChan.
-									// Using the ApproveToolChan we provide through the
-									// toolChunk, it will wait for a response from the
-									// main go routine (essentially the client or user)
-									// with a reject or approval of the tool. Following
-									// that, it will then run the tool and return the
-									// result through chunkChan which we provided.
-
-									go ToolManager(ctx, toolChunk, a.Tools[*tool.Function.Name], tool, chunkChan)
-
-									// After passing the tool into ToolManager, we can send
-									// it off to the client through chunkChan
-
-									chunkChan <- toolChunk
+								toolChunk := ToolChunk{
+									ID:              *tool.ID,
+									ToolName:        *tool.Function.Name,
+									ToolParams:      tool.Function.Arguments,
+									ApproveToolChan: ApproveToolChannel,
 								}
+
+								// Here we launch the ToolManager function. It takes
+								// in the toolChunk, the tool function, and the chunkChan.
+								// Using the ApproveToolChan we provide through the
+								// toolChunk, it will wait for a response from the
+								// main go routine (essentially the client or user)
+								// with a reject or approval of the tool. Following
+								// that, it will then run the tool and return the
+								// result through chunkChan which we provided.
+								go ToolManager(ctx, &a.Client, toolChunk, a.Tools[*tool.Function.Name], tool, chunkChan)
+
+								// After passing the tool into ToolManager, we can send
+								// it off to the client through chunkChan
+								chunkChan <- toolChunk
+
+							} else if slices.Contains(a.ToolBlacklist, *tool.Function.Name) || slices.Contains(a.ToolBlacklist, "*") {
+
+								// If the tool is in the blacklist (or the
+								// user added "*" to the blacklist, which
+								// automatically adds all tools), and
+								// not in the whitelist, then we send a
+								// AsyncAgentToolFailedChunk error to the client
+								// to tell them that the tool is blacklisted.
+								toolBlacklistedErrorChunk := ToolFailedChunk{
+									ID:         *tool.ID,
+									ToolName:   *tool.Function.Name,
+									ToolParams: tool.Function.Arguments,
+									Reason:     "tool in blacklist.",
+								}
+
+								chunkChan <- toolBlacklistedErrorChunk
+							} else {
+
+								// If the tool isn't found in either the blacklist
+								// or the whitelist, we simply send to back to the user.
+								ApproveToolChannel := make(chan bool)
+
+								toolChunk := ToolChunk{
+									ID:              *tool.ID,
+									ToolName:        *tool.Function.Name,
+									ToolParams:      tool.Function.Arguments,
+									ApproveToolChan: ApproveToolChannel,
+								}
+
+								// As above, we launch the ToolManager function. It takes
+								// in the toolChunk, the tool function, and the chunkChan.
+								// Using the ApproveToolChan we provide through the
+								// toolChunk, it will wait for a response from the
+								// main go routine (essentially the client or user)
+								// with a reject or approval of the tool. Following
+								// that, it will then run the tool and return the
+								// result through chunkChan which we provided.
+								go ToolManager(ctx, &a.Client, toolChunk, a.Tools[*tool.Function.Name], tool, chunkChan)
+
+								// After passing the tool into ToolManager, we can send
+								// it off to the client through chunkChan
+								chunkChan <- toolChunk
 							}
 						}
 					}
@@ -218,5 +202,7 @@ func (a *Agent) RunAsync(ctx schemas.BifrostContext, message string) <-chan any 
 		}
 	}()
 
+	// Finally, we return chunkChan to the parent where
+	// it can be processed.
 	return chunkChan
 }
--- agent/tools.go
+++ agent/tools.go
@@ -32,11 +32,10 @@ type Tools map[string]ToolFunc
 
 // ToolManager is the function that... well, manages tool
 // calls. It recieves the tool chunk (for the tool information),
-// the tool function (to be able to run the tool), and the
-// chunk channel to be able to communicate with the main
-// go routine.
-
-func ToolManager(ctx schemas.BifrostContext, tool ToolChunk, toolFunc ToolFunc, toolCall schemas.ChatAssistantMessageToolCall, chunkChan chan any) {
+// the tool function (to be able to run the tool), the tool call
+// (for the MCP tools), and the chunk channel to be able to
+// communicate with the main go routine.
+func ToolManager(ctx schemas.BifrostContext, client *bifrost.Bifrost, tool ToolChunk, toolFunc ToolFunc, toolCall schemas.ChatAssistantMessageToolCall, chunkChan chan any) {
 
 	// Here we use the ApproveToolChan located inside the ToolChunk
 	// to wait for the the user to confirm (or reject) the tool.
@@ -45,47 +44,70 @@ func ToolManager(ctx schemas.BifrostContext, tool ToolChunk, toolFunc ToolFunc, 
 	case approved := <-tool.ApproveToolChan:
 		if approved {
 
-			// If the user approves the tool, then we run the tool
-			// function.
-
-			// Set the response and err.
-
-			var response any
-			var err error
-
 			// Here we check whether a tool is an MCP tool or not.
 			// This will tell us whether to execute it as an actual
-			// function or through bifrost.ExecuteChatMCPTool.
+			// function or through the Bifrost client.
 			// The way we check is whether the tool name contains a "-".
 			// This is kinda bad and unweildy, as we have to ban users
 			// from creating any tool names with the character "-",
 			// but there is (currently) no other way that I have found.
 
 			if strings.Contains(tool.ToolName, "-") {
-				response, err = bifrost.ExecuteChatMCPTool(ctx, toolCall)
-			} else {
-				response, err = toolFunc(tool.ToolParams)
-			}
+				response, bifrostErr := client.ExecuteChatMCPTool(&ctx, &toolCall)
 
-			// If there is an error, send it to the client through
-			// the chunk chan.
+				if bifrostErr != nil {
+					chunkChan <- ToolFailedChunk{
+						ID:         tool.ID,
+						ToolName:   tool.ToolName,
+						ToolParams: tool.ToolParams,
+						Reason:     bifrostErr.String(),
+					}
 
-			if err != nil {
-				chunkChan <- ToolFailedChunk{
-					ID:         tool.ID,
-					ToolName:   tool.ToolName,
-					ToolParams: tool.ToolParams,
-					Reason:     err.Error(),
+					return
 				}
 
-				return
+				chunkChan <- response
+			} else {
+
+				var response ToolFuncResponse
+
+				// The tool function, gotten from the tool
+				// map, can be nil. Here we check for that.
+
+				if toolFunc != nil {
+
+					// If the function isn't nil, congrats and
+					// we run it.
+
+					response, err := toolFunc(tool.ToolParams)
+
+					if err != nil {
+						chunkChan <- ToolFailedChunk{
+							ID:         tool.ID,
+							ToolName:   tool.ToolName,
+							ToolParams: tool.ToolParams,
+							Reason:     err.Error(),
+						}
+
+						return
+					}
+
+					chunkChan <- response
+				} else {
+
+					// Otherwise, we send an error chunk notifying
+					// the parent about the failure.
+
+					chunkChan <- ToolFailedChunk{
+						ID:         tool.ID,
+						ToolName:   tool.ToolName,
+						ToolParams: tool.ToolParams,
+						Reason:     "tool function mismatch: tool function does not exist.",
+					}
+				}
+
+				chunkChan <- response
 			}
-
-			// If there is no error, then we send the tool response
-			// back to the client, where is can be further managed
-			// (such as add to chat history for the model to see).
-
-			chunkChan <- response
 
 			return
 		} else {
